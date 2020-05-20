f33c2bdef8a816d9abec410cd6563c41148923fd
Adam <adas.machaj@gmail.com>
2020-05-20 12:48:20 +0200

    Main.py changes

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 import tweepy
+from keys import keys
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("VkOPxi5RoGq16dJG5CtqDXJKM",
-    "Gngak0iL5AToolVKFuWAspWgCsRAyXDBXFNtFyCymC3zms53qf")
-auth.set_access_token("573833945-OA00farFXda3Lf1fbzMiOUsfzLkaagekDLvl6xnp",
-    "yCetWSP35wPjKbb4FM92iPHGOVN6QbFPUnQEv56a7RVZ3")
+auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
+auth.set_access_token(keys['access_token'], keys['access_token_secret'])
 
-api = tweepy.API(auth)
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-
-    # print(api.friends_ids('sleepyadamm'))
-
-    j = 1
-    for i in api.friends_ids('sleepyadamm'):
-        print(i)
-        # print(j, "   ", i)
-        # j+=1
-        # api.destroy_frienship(i)
-
-
-except:
-    print("Error during authentication")
+api = tweepy.API(auth, wait_on_rate_limit=True)
